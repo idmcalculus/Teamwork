7789d814a5e23c0b3af4f19a9cf2eefc48951114
Damilola Michael Ige <idm.calculus@gmail.com>
2019-11-23 21:50:44 +0100

    feature: employees can edit articles

---
+++ .history/server/app_20191123214834.js
@@ -0,0 +1,21 @@
+import "@babel/polyfill";
+import express from 'express';
+import bodyParser from 'body-parser';
+import users from './routes/user';
+import articles from './routes/articles';
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+app.use('/api/v1', users);
+app.use("/api/v1", articles);
+
+module.exports = app;
