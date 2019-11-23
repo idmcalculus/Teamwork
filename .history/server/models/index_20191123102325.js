743e4edfed5f683af247b3bd0a751fa7f3471f09
Damilola Michael Ige <idm.calculus@gmail.com>
2019-11-23 10:24:46 +0100

    set up database

---
+++ .history/server/models/index_20191123102325.js
@@ -0,0 +1,22 @@
+import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+let dbString = process.env.DB_URL;
+let client = new pg.Client(dbString);
+
+client.connect(function(err) {
+  if (err) {
+    return console.error("could not connect to postgres", err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if (err) {
+      return console.error("error running query", err);
+    }
+	console.log("Database Connected");
+	console.log(result.rows[0].theTime);
+  });
+});
+
+module.exports = client;
